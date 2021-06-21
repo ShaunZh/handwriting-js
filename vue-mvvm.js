8219b6ff390f17e8b839697905360fc8558a2fd4
shaunZh <308826842@qq.com>
2021-06-21 15:09:08 +0800

    update: vue-mvvm 发布订阅&添加watcher

--- vue-mvvm.js
+++ vue-mvvm.js
@@ -3,10 +3,11 @@
  * @Author: Hexon
  * @Date: 2021-06-21 11:26:39
  * @LastEditors: Hexon
- * @LastEditTime: 2021-06-21 14:46:41
+ * @LastEditTime: 2021-06-21 15:08:33
  */
 
 function defineReactive(obj, key, val) {
+  let dep = new Dep()
   observer(val);
   Object.defineProperty(obj, key, {
     enumerable: true,
@@ -19,6 +20,7 @@ function defineReactive(obj, key, val) {
         return;
       }
       console.log('哈哈哈，监听到值变化了 ', val, ' --> ', newVal);
+      dep.notify()
     }
   })
 }
@@ -32,6 +34,35 @@ function observer(value) {
   })
 }
 
+function Dep() {
+  this.deps = []
+}
+
+Dep.prototype.add = function (sub) {
+  this.deps.push(sub)
+}
+
+Dep.prototype.notify = function (v) {
+  this.deps.forEach(function (sub) {
+    sub.update(v)
+  })
+}
+
+// 监听函数：监听到数据改变后会调用update函数
+function Watcher(fn) {
+  this.fn = fn;
+}
+
+Watcher.prototype.update = function (v) {
+  this.fn(v);
+}
+
+const watcher = new Watcher(v => console.log('watcher ', v))
+
+const dep = new Dep()
+dep.add(watcher)
+dep.add(watcher)
+dep.notify('update')
 
 
 class Vue {
