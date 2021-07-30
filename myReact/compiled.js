bbab3f385c60ef28308bda6821df40c47e0ec50b
shaunZh <308826842@qq.com>
2021-07-30 13:53:17 +0800

    update: react-初步完成render方法(使用递归方式)

--- myReact/compiled.js
+++ myReact/compiled.js
@@ -3,17 +3,14 @@
  * @Author: Hexon
  * @Date: 2021-07-29 11:07:48
  * @LastEditors: Hexon
- * @LastEditTime: 2021-07-30 10:29:20
+ * @LastEditTime: 2021-07-30 13:50:06
  */
 function createElement(type, props, ...children) {
   return {
     type,
-    props: {
-      ...props,
-      children: children.map((child) =>
-        typeof child === "object" ? child : createTextElement(child)
-      ),
-    },
+    props: { ...props,
+      children: children.map(child => typeof child === "object" ? child : createTextElement(child))
+    }
   };
 }
 
@@ -22,20 +19,35 @@ function createTextElement(text) {
     type: "TEXT_ELEMENT",
     props: {
       nodeValue: text,
-      children: [],
-    },
+      children: []
+    }
   };
+}
+
+function render(element, container) {
+  const dom = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
+
+  const isProperty = key => key !== "children"; // 将非children属性添加到dom元素上
+
+
+  Object.keys(element.props).filter(isProperty).forEach(name => dom[name] = element.props[name]); // 递归渲染
+
+  element.props.children.forEach(child => render(child, dom));
+  container.appendChild(dom);
 }
 
 const Didact = {
   createElement,
+  render
 };
+const element = Didact.createElement("div", {
+  id: "foo"
+}, Didact.createElement("a", null, "bar"), Didact.createElement("b", null, "加粗123")); // const element2 = () => (
+//   <div id="foo">
+//     <a href="#">bar</a>
+//     <b>加粗</b>
+//   </div>
+// );
 
-const element = Didact.createElement(
-  "div",
-  {
-    id: "foo",
-  },
-  Didact.createElement("a", null, "bar"),
-  Didact.createElement("b")
-);
+const container = document.getElementById("root");
+Didact.render(element, container);
--- myReact/react.js
+++ myReact/react.js
@@ -3,7 +3,7 @@
  * @Author: Hexon
  * @Date: 2021-07-29 11:07:48
  * @LastEditors: Hexon
- * @LastEditTime: 2021-07-30 10:34:35
+ * @LastEditTime: 2021-07-30 13:50:06
  */
 
 function createElement(type, props, ...children) {
@@ -28,15 +28,42 @@ function createTextElement(text) {
   };
 }
 
+function render(element, container) {
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== "children";
+
+  // 将非children属性添加到dom元素上
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => (dom[name] = element.props[name]));
+
+  // 递归渲染
+  element.props.children.forEach((child) => render(child, dom));
+
+  container.appendChild(dom);
+}
+
 const Didact = {
   createElement,
+  render,
 };
 
 const element = Didact.createElement(
   "div",
   { id: "foo" },
   Didact.createElement("a", null, "bar"),
-  Didact.createElement("b")
+  Didact.createElement("b", null, "加粗123")
 );
 
+// const element2 = () => (
+//   <div id="foo">
+//     <a href="#">bar</a>
+//     <b>加粗</b>
+//   </div>
+// );
 const container = document.getElementById("root");
+Didact.render(element, container);
