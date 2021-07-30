3644a7bc74b44ffd8ddf2bae5db0000374392d91
shaunZh <308826842@qq.com>
2021-07-30 10:39:29 +0800

    add:  react - 初步实现createElement

---
+++ myReact/compiled.js
@@ -0,0 +1,41 @@
+/*
+ * @Description:
+ * @Author: Hexon
+ * @Date: 2021-07-29 11:07:48
+ * @LastEditors: Hexon
+ * @LastEditTime: 2021-07-30 10:29:20
+ */
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const Didact = {
+  createElement,
+};
+
+const element = Didact.createElement(
+  "div",
+  {
+    id: "foo",
+  },
+  Didact.createElement("a", null, "bar"),
+  Didact.createElement("b")
+);
