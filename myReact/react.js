c2ad94e68f293f0fcd24ef936aff6ceb602fc4ca
shaunZh <308826842@qq.com>
2021-08-01 13:54:28 +0800

    update: react-函数组件

--- myReact/react.js
+++ myReact/react.js
@@ -3,7 +3,7 @@
  * @Author: Hexon
  * @Date: 2021-07-29 11:07:48
  * @LastEditors: Hexon
- * @LastEditTime: 2021-07-31 16:37:49
+ * @LastEditTime: 2021-08-01 13:53:47
  */
 
 function createElement(type, props, ...children) {
@@ -90,17 +90,30 @@ function commitRoot() {
 function commitWork(fiber) {
   if (!fiber) return;
 
-  const domParent = fiber.parent.dom;
+  let domParentFiber = fiber.parent;
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent;
+  }
+
+  const domParent = domParentFiber.dom;
   if (fiber.effectTag === "PLACEMENT" && fiber.dom) {
     domParent.appendChild(fiber.dom);
   } else if (fiber.effectTag === "UPDATE" && fiber.dom) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props);
   } else if (fiber.effectTag === "DELETION") {
-    domParent.removeChild(fiber.dom);
+    commitDeletion(fiber, domParent);
   }
   // 递归添加所有的node到dom
   commitWork(fiber.child);
   commitWork(fiber.sibling);
+}
+
+function commitDeletion(fiber, domParent) {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom);
+  } else {
+    commitDeletion(fiber.child, domParent);
+  }
 }
 
 function render(element, container) {
@@ -150,14 +163,13 @@ requestIdleCallback(workLoop);
  * @return {*}
  */
 function performUnitOfWork(fiber) {
-  // 1. add the element to the DOM
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
+  const isFunctionComponent = fiber.type instanceof Function;
+
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber);
+  } else {
+    updateHostComponent(fiber);
   }
-
-  const elements = fiber.props.children;
-  reconcileChildren(fiber, elements);
-
   // 3. select the next unit of work
   if (fiber.child) {
     return fiber.child;
@@ -170,6 +182,21 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent;
   }
+}
+
+function updateFunctionComponent(fiber) {
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children);
+}
+
+function updateHostComponent(fiber) {
+  // 1. add the element to the DOM
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  const elements = fiber.props.children;
+  reconcileChildren(fiber, elements);
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -238,18 +265,25 @@ const Didact = {
 /** @jsx Didact.createElement */
 const container = document.getElementById("root");
 
-const updateValue = (e) => {
-  rerender(e.target.value);
-};
+function App(props) {
+  return <h1>Hi {props.name}</h1>;
+}
 
-const rerender = (value) => {
-  const element = (
-    <div>
-      <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
-    </div>
-  );
-  Didact.render(element, container);
-};
+const element = <App name="hexon"></App>;
+Didact.render(element, container);
 
-rerender("World");
+// const updateValue = (e) => {
+//   rerender(e.target.value);
+// };
+
+// const rerender = (value) => {
+//   const element = (
+//     <div>
+//       <input onInput={updateValue} value={value} />
+//       <h2>Hello {value}</h2>
+//     </div>
+//   );
+//   Didact.render(element, container);
+// };
+
+// rerender("World");
